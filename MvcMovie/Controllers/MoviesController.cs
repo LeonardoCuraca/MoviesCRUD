b1f4a0ea080b83c74110e98cb199f1cde3933625
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-11-17 23:38:00 -0500

    first commit

---
+++ MvcMovie/Controllers/MoviesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private MvcMovieContext db = new MvcMovieContext();
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>();
+            if (Session["Movies"] == null)
+            {
+                movies.Add(new Movie { ID = 1, Title = "Capitán América: El primer vengador", ReleaseDate = new DateTime(2011, 7, 28), Genre = "Acción", Price = 29.99M });
+                movies.Add(new Movie { ID = 2, Title = "Iron Man", ReleaseDate = new DateTime(2008, 5, 2), Genre = "Acción", Price = 29.99M });
+                Session["Movies"] = movies;
+            }
+            else
+            {
+                movies = ((List<Movie>)Session["Movies"]);
+            }
+            return View(movies);
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = ((List<Movie>)Session["Movies"]).Find(x => x.ID == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Movie movie)
+        {
+            try
+            {
+                ((List<Movie>)Session["Movies"]).Add(movie);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(movie);
+            }
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = ((List<Movie>)Session["Movies"]).Find(x => x.ID == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                ((List<Movie>)Session["Movies"])[((List<Movie>)Session["Movies"]).FindIndex(x => x.ID == movie.ID)] = movie;
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = ((List<Movie>)Session["Movies"]).Find(x => x.ID == id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ((List<Movie>)Session["Movies"]).RemoveAt(((List<Movie>)Session["Movies"]).FindIndex(x => x.ID == id));
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
